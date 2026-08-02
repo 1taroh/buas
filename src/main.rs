ee38438465842db29ee786133e5e23663c4d4f01
1taroh <65810214+1taroh@users.noreply.github.com>
2026-08-02 14:45:06 +0900

    絶対パスにすることでパスの移動に対してロバストにする

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::path::Path;
 use std::process::Command;
 use uuid::Uuid;
 
@@ -12,6 +13,13 @@ fn main() -> std::io::Result<()> {
     fs::create_dir_all(&dir)?;
 
     let program = args.next().expect("コマンドが指定されていません");
+    let program_path = Path::new(&program);
+    let program = if program_path.is_absolute() || !program.contains('/') {
+        program.into()
+    } else {
+        std::env::current_dir()?.join(program)
+    };
+
     let status = Command::new(program)
         .args(args)
         .current_dir(&dir)
